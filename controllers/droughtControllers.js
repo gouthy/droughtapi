37a364ae52c7c5de4155146a40514cef9da3484a
Goutam Konapala <goutam.iitbbs@gmail.com>
2023-04-19 08:18:10 -0400

    added type backblaze accounts to support backend

--- controllers/droughtControllers.js
+++ controllers/droughtControllers.js
@@ -3,17 +3,40 @@
 //@route  GET/api/droughtstatus/:lat/:lon
 //@access public 
 
+const asyncHandler = require("express-async-handler");
+const dotenv = require("dotenv").config();
 
-const getSingleLocationData = (req, res) => {
+const AWS = require('aws-sdk');
+
+
+
+const getSingleLocationData = asyncHandler(async (req, res) => {
     const lat = req.params.lat;
     const lon = req.params.lon;
+    const s3 = new AWS.S3({
+        accessKeyId: process.env.KEY_ID,
+        secretAccessKey: process.env.PASS_KEY,
+        endpoint: process.env.end_point,
+        s3ForcePathStyle: true,
+        signatureVersion: 'v4'
+      });
+      
+      const s3Params = {
+          Bucket: 'temmdata',
+          Key: 'droughtdata.tiff'
+        };
+      
+      const s3Data = await s3.getObject(s3Params).promise();
+
+      const geotiffData = await geoblaze.identify(s3Data.Body,[lat,lon]);
+      
     if (isNaN(Number(lat)) || isNaN(Number(lon))) {
         res.status(400);
         throw new Error("The input should be a number")
     }
 
-    res.status(200).json({message:`get data for ${lat} and ${lon}`});
-};
+    res.status(200).json({message:geotiffData});
+});
 
 module.exports = {getSingleLocationData};
 
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 
 const express = require('express');
+const errorHandler = require('./middleware/errorHandler');
 const dotenv = require("dotenv").config();
 
 const port = process.env.PORT ;
 
+console.log(process.env.KEY_ID)
 const app = express(); 
 
 app.use(express.json());
 app.use('/api/droughtstatus', require('./routes/droughtRoutes'));
-
+app.use(errorHandler);
 app.listen(port, () => console.log(`Server running on port ${port}`));
